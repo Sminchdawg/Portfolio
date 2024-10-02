d8152518fd032839eef29fb62b3fecb92f590fdb
Mitchell Genova <magenova12@gmail.com>
2024-10-02 14:51:37 -0400

    Trying out opening up the keyboard for mobile users so they can play the Dino game when hitting the secret Dino page

--- src/app/dino/page.tsx
+++ src/app/dino/page.tsx
@@ -3,8 +3,18 @@
 import { Dino } from "react-dino-game";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 export default function DinoPage() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // Focus the input field when the page is rendered, this is for mobile devices to work with the game
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
     <div className="flex items-center flex-col">
       <div className="flex items-center text-3xl text-background mb-5 gap-5">
@@ -15,6 +25,11 @@ export default function DinoPage() {
       </div>
       <div className="bg-background max-sm:[&_.game]:w-[350px] max-md:[&_.game]:w-[450px] [&_.game]:max-w-[600px]">
         <Dino />
+        <input
+          ref={inputRef}
+          style={{ opacity: 0, position: "absolute", left: "-9999px" }}
+          type="text"
+        />
       </div>
     </div>
   );
